40072d83072312fb88054c40b904977c1b66944b
Danny van Wijk <danny@insiders.nl>
2019-12-09 11:52:43 +0000

    Fix deprecations

--- Controller/ApiDocController.php
+++ Controller/ApiDocController.php
@@ -11,33 +11,46 @@
 
 namespace Nelmio\ApiDocBundle\Controller;
 
+use Nelmio\ApiDocBundle\Extractor\ApiDocExtractor;
+use Nelmio\ApiDocBundle\Formatter\HtmlFormatter;
 use Nelmio\ApiDocBundle\Formatter\RequestAwareSwaggerFormatter;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Nelmio\ApiDocBundle\Formatter\SwaggerFormatter;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class ApiDocController extends Controller
+class ApiDocController
 {
+    private $apiDocExtractor;
+    private $htmlFormatter;
+    private $swaggerFormatter;
+
+    public function __construct(ApiDocExtractor $apiDocExtractor, HtmlFormatter $htmlFormatter, SwaggerFormatter $swaggerFormatter)
+    {
+        $this->apiDocExtractor = $apiDocExtractor;
+        $this->htmlFormatter = $htmlFormatter;
+        $this->swaggerFormatter = $swaggerFormatter;
+    }
+
     public function indexAction($view = ApiDoc::DEFAULT_VIEW)
     {
-        $extractedDoc = $this->get('nelmio_api_doc.extractor.api_doc_extractor')->all($view);
-        $htmlContent  = $this->get('nelmio_api_doc.formatter.html_formatter')->format($extractedDoc);
+        $extractedDoc = $this->apiDocExtractor->all($view);
+        $htmlContent  = $this->htmlFormatter->format($extractedDoc);
 
         return new Response($htmlContent, 200, array('Content-Type' => 'text/html'));
     }
 
     public function swaggerAction(Request $request, $resource = null)
     {
-
-        $docs = $this->get('nelmio_api_doc.extractor.api_doc_extractor')->all();
-        $formatter = new RequestAwareSwaggerFormatter($request, $this->get('nelmio_api_doc.formatter.swagger_formatter'));
+        $docs = $this->apiDocExtractor->all();
+        $formatter = new RequestAwareSwaggerFormatter($request, $this->swaggerFormatter);
 
         $spec = $formatter->format($docs, $resource ? '/' . $resource : null);
 
         if ($resource !== null && count($spec['apis']) === 0) {
-            throw $this->createNotFoundException(sprintf('Cannot find resource "%s"', $resource));
+            throw new NotFoundHttpException(sprintf('Cannot find resource "%s"', $resource));
         }
 
         return new JsonResponse($spec);
